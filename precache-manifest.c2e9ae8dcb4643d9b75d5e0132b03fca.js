a9b19e303be43d0cdab70760f996b93c3af3d1a1
Tony CoderYo <abundantant@gmail.com>
2019-03-05 13:16:46 -0500

    Updates

---
+++ precache-manifest.c2e9ae8dcb4643d9b75d5e0132b03fca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "8e6e54cb26a4cf650e6c",
+    "url": "/Clicky-Game/static/js/main.8e6e54cb.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/Clicky-Game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "2b9fbb0f09d438556eee3a365f090bc4",
+    "url": "/Clicky-Game/index.html"
+  }
+];
